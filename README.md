f0ed537d9ff36f2fed91f226374b49683ef3e261
Peter Crabbe <petercrabbe92@gmail.com>
2020-06-10 01:47:00 +0800

    Update CMake script and readme

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # LuaAndLuaBridgeTemplate
 A simple C++ Lua and Luabridge project for experimenting with.
+https://eliasdaler.wordpress.com/2014/07/18/using-lua-with-cpp-luabridge/
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,22 @@
 #include <iostream>
+extern "C" {
+# include "lua.h"
+# include "lauxlib.h"
+# include "lualib.h"
+}
+//You must include lua headers first before LuaBridge
+#include <LuaBridge/LuaBridge.h>
+ 
+using namespace luabridge;
 int main() {
-    std::cout << "Hello World";
-    return 0;
+    lua_State* L = luaL_newstate();
+    luaL_dofile(L, "scripts/script.lua");
+    luaL_openlibs(L);
+    lua_pcall(L, 0, 0, 0);
+    LuaRef s = getGlobal(L, "testString");
+    LuaRef n = getGlobal(L, "number");
+    auto luaString = s.cast<std::string>();
+    int answer = n.cast<int>();
+    std::cout << luaString << std::endl;
+    std::cout << "And here's our number:" << answer << std::endl;
 }
